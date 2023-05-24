e832ae74007dd6612704a97d74941d98968a9313
Mani <125630807+ManiShankar02@users.noreply.github.com>
2023-05-24 12:42:47 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  <div className = "container">
+    <h1 className = "head"> Congratulations</h1>
+    <div className = "subContainer">
+      <img src = "https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" className = "image";
+      <h1 className = "heading"> Mani Shankar</h1>
+      <p> Kakatiya university, Warangal</p>
+      <img src = "https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" className = "image";
+     </div>
+
+  </div>
+  );
+  
+ReactDOM.render(element, document.getElementById('root'));
